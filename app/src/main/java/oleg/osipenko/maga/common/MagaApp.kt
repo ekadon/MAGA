b280f4e5969531e52be9fdf72c6d34f731ef16d8
olegosipenko <oleg.s.osipenko@gmail.com>
2018-11-13 13:00:51 +0100

    Strict mode

--- app/src/main/java/oleg/osipenko/maga/common/MagaApp.kt
+++ app/src/main/java/oleg/osipenko/maga/common/MagaApp.kt
@@ -1,15 +1,45 @@
 package oleg.osipenko.maga.common
 
 import android.app.Application
+import android.os.StrictMode
 import com.jakewharton.threetenabp.AndroidThreeTen
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import oleg.osipenko.maga.data.di.dataModule
 import oleg.osipenko.maga.mainactivity.MainActivity
 import org.koin.android.ext.android.startKoin
 
 class MagaApp : Application() {
   override fun onCreate() {
+    val vmPolicy =
+        StrictMode.VmPolicy.Builder()
+          .detectActivityLeaks()
+          .detectCleartextNetwork()
+          .detectContentUriWithoutPermission()
+          .detectFileUriExposure()
+          .detectLeakedClosableObjects()
+          .detectLeakedRegistrationObjects()
+          .detectLeakedSqlLiteObjects()
+          .penaltyLog()
+          .penaltyDeath()
+          .build()
+    StrictMode.setVmPolicy(vmPolicy)
+    val tPolicy =
+        StrictMode.ThreadPolicy.Builder()
+          .detectDiskWrites()
+          .detectCustomSlowCalls()
+          .detectNetwork()
+          .detectResourceMismatches()
+          .detectUnbufferedIo()
+          .penaltyLog()
+          .penaltyDeath()
+          .build()
+    StrictMode.setThreadPolicy(tPolicy)
+
     super.onCreate()
-    AndroidThreeTen.init(this)
+    GlobalScope.launch {
+      AndroidThreeTen.init(applicationContext)
+    }
 
     startKoin(this, listOf(dataModule, MainActivity.activityModel))
   }
