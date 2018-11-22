e869edbfa8ec6118c29a1793ac9fb0418c788ec3
olegosipenko <oleg.s.osipenko@gmail.com>
2018-11-22 17:17:37 +0100

    inlined filtering functions

--- data/src/main/java/oleg/osipenko/maga/data/repository/NowPlayingDateFilter.kt
+++ data/src/main/java/oleg/osipenko/maga/data/repository/NowPlayingDateFilter.kt
@@ -4,8 +4,8 @@ import oleg.osipenko.maga.data.entities.MovieRecord
 import org.threeten.bp.LocalDate
 import org.threeten.bp.temporal.ChronoUnit
 
-fun nowPlayingDateFilter(
-    currentDate: LocalDate, movieRecord: MovieRecord
+inline fun nowPlayingDateFilter(
+  currentDate: LocalDate, movieRecord: MovieRecord
 ): Boolean {
   val movieDate = LocalDate.parse(movieRecord.releaseDate)
   val daysSinceRelease = ChronoUnit.DAYS.between(
--- data/src/main/java/oleg/osipenko/maga/data/repository/UpcomingDateFilter.kt
+++ data/src/main/java/oleg/osipenko/maga/data/repository/UpcomingDateFilter.kt
@@ -4,8 +4,9 @@ import oleg.osipenko.maga.data.entities.MovieRecord
 import org.threeten.bp.LocalDate
 import org.threeten.bp.Period
 
-fun upcomingDateFilter(currentDate: LocalDate, movieRecord: MovieRecord):
-Boolean {
+inline fun upcomingDateFilter(
+  currentDate: LocalDate, movieRecord: MovieRecord
+): Boolean {
   val movieDate = LocalDate.parse(movieRecord.releaseDate)
   val dateDelta = Period.between(
     currentDate, movieDate
